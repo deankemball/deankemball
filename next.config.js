53c3279c39418a6aa4b954d1cfe5da6921b68e9e
Dean Wallflower <dean@deanwallflower.com>
2022-06-19 00:42:02 +0200

    fix next.config again

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["netlify.com", "app.netlify.com"],
+    domains: ["netlify.app", "deankemball.netlify.app"],
   },
   reactStrictMode: true,
   webpack(config) {
