b3a060afdeda120c6fbc7dd177d45245ff6bc8d0
Dean Wallflower <dean@deanwallflower.com>
2022-06-28 21:33:58 +0200

    change filepath for media in carousel

--- components/Carousel.tsx
+++ components/Carousel.tsx
@@ -8,17 +8,17 @@ import { motion } from "framer-motion";
 
 const Projects = [
   {
-    image: "/../public/media-1.jpg",
+    image: "/media-1.jpg",
     title: "Dean Wallflower",
     link: "https://deanwallflower.com/",
   },
   {
-    image: "/../public/media-2.jpg",
+    image: "/media-2.jpg",
     title: "MERCH BABE",
     link: "https://merchbabe.net/",
   },
   {
-    image: "/../public/media-3.jpg",
+    image: "/media-3.jpg",
     title: "Dean Wallflower",
     link: "https://dw-tw.netlify.com/",
   },
