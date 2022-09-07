1e7b7218a3759d8fa139ba34d2d82da44841090a
Dean Wallflower <dean@deanwallflower.com>
2022-09-07 19:57:47 +0200

    test fixed footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -16,15 +16,15 @@ export default function Layout({ children }: any) {
         }}
       >
         <div className="min-h-screen w-screen bg-white dark:bg-black flex-col flex justify-between z-50">
-          <div className="flex-col space-y-2 absolute left-8 top-0 pt-6 z-50">
+          <div className="flex-col space-y-2 fixed left-8 top-6 z-50">
             <Header />
           </div>
-          <div className="absolute left-8 bottom-0 pb-6">
+          <div className="fixed left-8 bottom-6">
             <ThemeSwitch />
           </div>
 
           <main className="overflow-y-scroll">{children}</main>
-          <div className="flex-col space-y-2 absolute right-8 bottom-0 pb-6">
+          <div className="flex-col space-y-2 fixed right-8 bottom-6">
             <Footer />
           </div>
         </div>
