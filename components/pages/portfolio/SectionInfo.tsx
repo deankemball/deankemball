e4862c45752478505d3a30d95b19b9ca1812a271
Dean Wallflower <dean@deanwallflower.com>
2022-09-08 15:51:36 +0200

    centered columns

--- components/pages/portfolio/SectionInfo.tsx
+++ components/pages/portfolio/SectionInfo.tsx
@@ -12,43 +12,47 @@ interface SectionInfoProps {
 
 const SectionInfo = ({ bgColor, textColor, paragraphs }: SectionInfoProps) => {
   return (
-    <section className={clsx("h-screen", bgColor, textColor)}>
-      <div className="flex flex-col lg:flex-row h-full w-screen items-center justify-start lg:justify-center lg:gap-16 py-24 px-8 lg:py-32 overflow-y-scroll lg:overflow-y-hidden">
-        <div className="flex flex-col font-ibm w-full max-w-md lg:max-w-lg gap-2">
-          <div className="flex w-full justify-between">
-            <div className="text-7xl">
-              <Right />
-            </div>
-            <h1 className="font-bold text-5xl lg:text-7xl leading-[52px] lg:leading-[60px]">
-              info
-            </h1>
-            <div className="text-7xl">
-              <Left />
-            </div>
+    <div
+      className={clsx(
+        "h-screen w-screen px-32 text-2xl lg:text-4xl leading-8 lg:leading-8 font-ibm font-thin flex flex-col justify-center",
+        bgColor,
+        textColor
+      )}
+    >
+      <div className="columns-xl gap-8">
+        <div className="flex w-full justify-center gap-8">
+          <div className="text-7xl">
+            <Right />
           </div>
-          <div className="flex flex-col gap-6 text-2xl lg:text-4xl leading-8 lg:leading-8 font-thin">
-            {paragraphs.map((paragraph, i) => {
-              return <p key={i}>{paragraph}</p>;
-            })}
-            <div className="flex w-full justify-between">
-              <div className="text-7xl">
-                <Right />
-              </div>
-              <Link href="https://www.merchbabe.net/">
-                <a>
-                  <h1 className="font-bold text-5xl lg:text-7xl leading-[52px] lg:leading-[60px]">
-                    link
-                  </h1>
-                </a>
-              </Link>
-              <div className="text-7xl">
-                <Left />
-              </div>
-            </div>
+          <h1 className="font-bold text-5xl lg:text-7xl leading-[52px] lg:leading-[60px]">
+            info
+          </h1>
+          <div className="text-7xl">
+            <Left />
+          </div>
+        </div>
+        <div className="flex flex-col gap-4">
+          {paragraphs.map((paragraph, i) => {
+            return <p key={i}>{paragraph}</p>;
+          })}
+        </div>
+        <div className="flex w-full justify-center gap-8">
+          <div className="text-7xl">
+            <Right />
+          </div>
+          <Link href="https://www.merchbabe.net/">
+            <a>
+              <h1 className="font-bold text-5xl lg:text-7xl leading-[52px] lg:leading-[60px]">
+                link
+              </h1>
+            </a>
+          </Link>
+          <div className="text-7xl">
+            <Left />
           </div>
         </div>
       </div>
-    </section>
+    </div>
   );
 };
 
