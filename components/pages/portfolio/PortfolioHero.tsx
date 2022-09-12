5712d285041865440fff4d1eee0942889a69829b
Dean Wallflower <dean@deanwallflower.com>
2022-09-12 17:56:29 +0200

    add icons to portfolio page

--- components/pages/portfolio/PortfolioHero.tsx
+++ components/pages/portfolio/PortfolioHero.tsx
@@ -1,5 +1,9 @@
 import Link from "next/link";
 import React from "react";
+import Fire from "../../../public/icons/fire.svg";
+import Carrot from "../../../public/icons/carrot.svg";
+import Game from "../../../public/icons/game.svg";
+import Camera from "../../../public/icons/camera.svg";
 
 const projects = [
   {
@@ -7,24 +11,28 @@ const projects = [
     link: "/portfolio/merchbabe",
     type: "artist portfolio & e-commerce",
     year: "2022",
+    icon: Fire,
   },
   {
     title: "cell-dodger.app",
     link: "https://cell-dodger.vercel.app/",
     type: "online game project",
     year: "2022",
+    icon: Game,
   },
   {
     title: "recipe app",
     link: "/portfolio/recipeapp",
     type: "ui/ux mockup",
     year: "2022",
+    icon: Carrot,
   },
   {
     title: "deanwallflower.com",
     link: "https://deanwallflower.com/",
     type: "artist portfolio",
     year: "2020",
+    icon: Camera,
   },
 ];
 
@@ -35,23 +43,37 @@ const Portfolio = () => {
         {projects.map((project, i) => {
           return (
             <>
-              <Link key={i} href={project.link}>
-                <a
-                  target={project.link.includes("http") ? "_blank" : ""}
-                  className="flex flex-col h-full justify-between hover:text-red dark:hover:text-red"
-                >
-                  <p className="text-2xl md:text-3xl lg:text-4xl decoration-red underline underline-offset-2">
-                    {project.title}
-                  </p>
-                  <p className="w-52 font-light text-xl md:text-2xl lg:text-3xl leading-5 md:leading-6 lg:leading-7">
-                    {project.type}
-                  </p>
-                  <div className="flex grow"></div>
-                  <p className="w-48 font-light text-lg md:text-xl lg:text-2xl leading-5 md:leading-6 lg:leading-7">
-                    {project.year}
-                  </p>
-                </a>
-              </Link>
+              <div className="flex flex-col h-full">
+                <Link key={i} href={project.link}>
+                  <a
+                    target={project.link.includes("http") ? "_blank" : ""}
+                    className="flex flex-col h-full justify-between"
+                  >
+                    <div className="flex gap-2 hover:text-red dark:hover:text-red">
+                      <div className="text-3xl flex items-center justify-center pt-2">
+                        {<project.icon />}
+                      </div>
+                      <p className="text-2xl md:text-3xl lg:text-4xl decoration-red underline underline-offset-2">
+                        {project.title}
+                      </p>
+                    </div>
+                  </a>
+                </Link>
+                <div className="flex gap-2">
+                  <div className="text-3xl invisible transition-colors duration-500 flex items-start pt-2">
+                    {<project.icon />}
+                  </div>
+                  <div className="flex flex-col select-none">
+                    <p className="w-52 font-light text-xl md:text-2xl lg:text-3xl leading-5 md:leading-6 lg:leading-7">
+                      {project.type}
+                    </p>
+                    <div className="flex grow"></div>
+                    <p className="w-48 font-light text-lg md:text-xl lg:text-2xl leading-5 md:leading-6 lg:leading-7">
+                      {project.year}
+                    </p>
+                  </div>
+                </div>
+              </div>
             </>
           );
         })}
