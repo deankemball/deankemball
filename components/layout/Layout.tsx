017c7b5651d055c01f841a248d0ce8c1750f26dd
Dean Kemball <deankemball@gmail.com>
2024-09-30 18:57:05 +0200

    add analytics

--- components/layout/Layout.tsx
+++ components/layout/Layout.tsx
@@ -5,7 +5,7 @@ import ThemeSwitch from "./ThemeSwitch";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import BackButton from "./BackButton";
-// import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function Layout({ children }: any) {
   const router = useRouter();
@@ -53,7 +53,7 @@ export default function Layout({ children }: any) {
             <Footer setSelected={setSelected} selected={selected} />
           </div>
         </div>
-        {/* <Analytics /> */}
+        <Analytics />
       </ThemeProvider>
     </>
   );
